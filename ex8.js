446136275c013e9b2d8b5798d6e3b608c8b8a280
unknown <karimkgp@gmail.com>
2021-03-31 15:34:31 +0200

    parseFloat añadido

--- ex8.js
+++ ex8.js
@@ -3,19 +3,19 @@ function operacions(op) {
 
     const ops = {
         sumar: function sumarNumeros(n1, n2) {
-            return (parseInt(n1) + parseInt(n2));
+            return (parseFloat(n1) + parseFloat(n2));
         },
 
         restar: function restarNumeros(n1, n2) {
-            return (parseInt(n1) - parseInt(n2));
+            return (parseFloat(n1) - parseFloat(n2));
         },
 
         multiplicar: function multiplicarNumeros(n1, n2) {
-            return (parseInt(n1) * parseInt(n2));
+            return (parseFloat(n1) * parseFloat(n2));
         },
 
         dividir: function dividirNumeros(n1, n2) {
-            return (parseInt(n1) / parseInt(n2));
+            return (parseFloat(n1) / parseFloat(n2));
         }
 
     };

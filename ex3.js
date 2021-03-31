bfdb0603c5f8df356d4c007215dc6a858a2dce35
unknown <karimkgp@gmail.com>
2021-03-31 11:44:37 +0200

    suma

--- ex3.js
+++
@@ -1,9 +0,0 @@
-let nombre = 'Karim ';
-let apellido = 'Ghazal';
-
-console.log(nombre+apellido);
-
-/*let nombre = 'Karim';
-let apellido = 'Ghazal';
-let espacio = ' ';
-console.log(nombre+espacio+apellido);*/
---
+++ ex4.js
@@ -0,0 +1,5 @@
+let numero1 = 5;
+let numero2 = 10;
+let suma = numero1+numero2;
+console.log(`La suma entre ${numero1} y ${numero2} es `+suma);
+

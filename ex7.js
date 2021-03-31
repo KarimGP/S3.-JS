5bf1cdebbaa621e11e87e692241243e1be25131c
unknown <karimkgp@gmail.com>
2021-03-31 13:36:12 +0200

    objectes i posició

---
+++ ex7.js
@@ -0,0 +1,6 @@
+var objectes = ['taula', 'cadira', 'ordinador', 'llibreta'];
+var contador;
+
+for( contador=0; contador < 4; contador++ ) {
+    console.log("L'objecte " + objectes[contador] + " es troba a la posició " + contador + ".");
+}

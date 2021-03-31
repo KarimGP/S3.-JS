7556786a66e6eb25e2a8dea05987411dcc116776
unknown <karimkgp@gmail.com>
2021-03-31 15:16:15 +0200

    calculadora simple

--- ex7.js
+++
@@ -1,6 +0,0 @@
-var objectes = ['taula', 'cadira', 'ordinador', 'llibreta'];
-var contador;
-
-for( contador=0; contador < 4; contador++ ) {
-    console.log("L'objecte " + objectes[contador] + " es troba a la posició " + contador + ".");
-}
---
+++ ex8.js
@@ -0,0 +1,57 @@
+
+function operacions(op) {
+
+    const ops = {
+        sumar: function sumarNumeros(n1, n2) {
+            return (parseInt(n1) + parseInt(n2));
+        },
+
+        restar: function restarNumeros(n1, n2) {
+            return (parseInt(n1) - parseInt(n2));
+        },
+
+        multiplicar: function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));
+        },
+
+        dividir: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
+        }
+
+    };
+
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+
+    //Comprobamos si se ha introducido números en las cajas
+    if (isNaN(parseFloat(document.getElementById('valor1').value))) {
+        document.getElementById('resultado').innerHTML = "<span style='color: red;'>Por favor, escriba un número 1</span>";
+        document.getElementById("valor1").innerText = "0";
+        document.getElementById("valor1").focus();
+    } else if (isNaN(parseFloat(document.getElementById('valor2').value))) {
+        document.getElementById('resultado').innerHTML = "<span style='color: red;'>Por favor, escriba un número 2</span>";
+        document.getElementById("valor2").innerText = "0";
+        document.getElementById("valor2").focus();
+    }
+    else {
+        //Si se han introducido los números en ámbas cajas, operamos:
+        switch (op) {
+            case 'sumar':
+                var resultado = ops.sumar(valor1, valor2);
+                document.getElementById('resultado').innerHTML = "<span style='color: green;'>" + resultado + "</span>";
+                break;
+            case 'restar':
+                var resultado = ops.restar(valor1, valor2);
+                document.getElementById('resultado').innerHTML = "<span style='color: green;'>" + resultado + "</span>";
+                break;
+            case 'multiplicar':
+                var resultado = ops.multiplicar(valor1, valor2);
+                document.getElementById('resultado').innerHTML = "<span style='color: green;'>" + resultado + "</span>";
+                break;
+            case 'dividir':
+                var resultado = ops.dividir(valor1, valor2);
+                document.getElementById('resultado').innerHTML = "<span style='color: green;'>" + resultado + "</span>";
+                break;
+        }
+    }
+}
